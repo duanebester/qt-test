e99a0e477274347d725fde17b664b5f6be66960f
Duane Bester <duane@besterdesigns.com>
2013-11-30 19:58:07 -0600

    init

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <QApplication>
+#include "html5applicationviewer.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    Html5ApplicationViewer viewer;
+    viewer.setOrientation(Html5ApplicationViewer::ScreenOrientationAuto);
+    viewer.showExpanded();
+    viewer.resize(640,480);
+    viewer.loadFile(QLatin1String("html/index.html"));
+
+    return app.exec();
+}
